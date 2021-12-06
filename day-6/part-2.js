b524b6d854b6e5e04176a8953edfce14908964a2
Lucas Henrique <lucashenrique580@gmail.com>
2021-12-06 15:46:11 -0300

    solutions for day 6

--- day-6/part-2.js
+++ day-6/part-2.js
@@ -7,12 +7,12 @@ const main = async () => {
   const initialState = inputFile.toString().split(',').map(Number);
   let newGenerationCount = 0;
   let population = 0;
-  let fishCountByTimerTick = [0, 0, 0, 0, 0, 0, 0, 0, 0];     // stores how much fish there is in in each time tick, from 0 (the was who will create a new generation) to 9 (the newborns). The newborns start at 9 because thei don't decrease the tick at first, only after the second day of life.
+  let fishCountByTimerTick = [0, 0, 0, 0, 0, 0, 0, 0, 0];     // stores how much fish there is in each time tick, from 0 (the ones who will create a new generation) to 9 (the newborns). The newborns start at 9 because they don't decrease the tick at first, only after the second day of life.
 
   initialState.forEach(tick => fishCountByTimerTick[tick]++); // initialize the counters with the tickers received as input
 
   for (let i = 0; i < 256; i++) {
-    fishCountByTimerTick[7] += fishCountByTimerTick[0];       // everyone that hits the 0 tick will restart with the timer at 6. Here, it goes to 7 because we still need to decrease the tickers, so they go with the ones currently at 7.
+    fishCountByTimerTick[7] += fishCountByTimerTick[0];       // everyone that hits 0 will restart with the timer at 6. Here, they go to 7 because we still need to decrease the tickers, so they can go with the ones currently at 7.
     newGenerationCount = fishCountByTimerTick.shift();        // decreases the ticker of everyone and gets the ones who was at 0.
     fishCountByTimerTick.push(newGenerationCount);            // warps the ones who hit 0 to the end (tick 9), creating a new generation.
   }
